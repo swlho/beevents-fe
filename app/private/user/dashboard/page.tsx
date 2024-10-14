960ee7eb3499fa9d5c5bd069fc6317b3441bb2b1
swlho <swlho87@gmail.com>
2024-10-14 11:53:42 +0800

    fix: add layout and auth login to private pages

--- app/private/user/dashboard/page.tsx
+++ app/private/user/dashboard/page.tsx
@@ -1,5 +1,3 @@
-"use server"
-
 import BackButton from "@/components/navigation/BackButton"
 import { redirect } from 'next/navigation'
 
@@ -7,21 +5,12 @@ import { createClient } from '@/utils/supabase/server'
 import Dashboard from "@/components/dashboard/Dashboard"
 
   
-export default async function UserDashboardPage() {
+export default function UserDashboardPage() {
 
-  const supabase = createClient()
-
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    redirect('/login')
-  }
-
-  if (data) {
-    return (
+  return (
       <>
       <BackButton path={"javascript:history.go(-1)"} />
       <Dashboard />
       </>
     )
-  }
 }
